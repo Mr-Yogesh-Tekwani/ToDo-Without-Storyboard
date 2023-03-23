62149c9b87cf9263db70baac97fd313ded015c0e
YOGESH TEKWANI <mryogeshtekwani@gmail.com>
2023-03-23 18:41:18 -0400

    Storyboard deleted

---
+++ TodoWithoutStoryboard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TodoWithoutStoryboard
+//
+//  Created by Yogesh Tekwani on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        self.view.backgroundColor = .red
+        
+        
+        
+    
+    
+    }
+
+
+}
+
